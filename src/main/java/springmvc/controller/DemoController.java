036871370eacd88a22aacf16c71d5094e4b2d3db
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-01-15 14:10:47 +0800

    Add files via upload

--- src/main/java/springmvc/controller/DemoController.java
+++ src/main/java/springmvc/controller/DemoController.java
@@ -1,6 +1,9 @@
 package springmvc.controller;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -57,4 +60,52 @@ public class DemoController {
 		double bmi = w / Math.pow(h/100, 2);
 		return String.format("bmi: %.2f", bmi);
 	}
+	
+	// 路徑參數 PathVariable
+	// 子路徑: /demo/exam/75 -> 結果: 75 pass
+	// 子路徑: /demo/exam/45 -> 結果: 45 fail
+	@RequestMapping("/exam/{score}")
+	@ResponseBody
+	public String exam(@PathVariable("score") Integer score) {
+		return String.format("score: %d %s", score, (score>=60)?"Pass":"Fail");
+	}
+	
+	/*
+	 * Lab: PathVariable + RequestParam
+	 * add 表示加法, sub 表示減法
+	 * 路徑：/demo/calc/add?x=30&y=20  -> 結果：50
+	 * 路徑：/demo/calc/sub?x=30&y=20  -> 結果：10
+	 * 路徑：/demo/calc/sub?y=20       -> 結果：20
+	 * 路徑：/demo/calc/sub?x=0&y=20   -> 結果：-20
+	 * 路徑：/demo/calc/add            -> 結果：0
+	 * 路徑：/demo/calc/abc            -> 結果："None" <- 無此路徑
+	 * 提示:可以使用 Optional<Integer> 來取代 Integer
+	 * 配置檔要加上 <mvc:annotation-driven /> 才可以使用 Optional<Integer>
+	 * */
+	@RequestMapping("/calc/{exp}")
+	@ResponseBody
+	public String calc(@PathVariable("exp") String exp,
+					@RequestParam(value = "x", required = false) Optional<Integer> x,
+					@RequestParam(value = "y", required = false) Optional<Integer> y) {
+		if(x.isPresent() && y.isPresent()) {
+			switch (exp) {
+				case "add":
+					return x.get() + y.get() + "";
+					
+				case "sub":
+					return x.get() - y.get() + "";
+							
+				default:
+					return "None";
+			}
+		}
+		if (!exp.equals("add") || !exp.equals("sub")) return "None";
+		if(x.isPresent()) return x.get() + "";
+		if(y.isPresent()) return y.get() + "";
+		return "0";
+	}
+	
+	
+	
+	
 }

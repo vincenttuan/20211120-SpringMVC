997514862346661cfe2f5f214ff357c859d04c0b
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-02-19 18:09:50 +0800

    Add files via upload

--- src/main/java/springmvc/lab/controller/EmpController.java
+++ src/main/java/springmvc/lab/controller/EmpController.java
@@ -1,11 +1,18 @@
 package springmvc.lab.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import springmvc.lab.entity.Employee;
@@ -23,4 +30,59 @@ public class EmpController {
 		model.addAttribute("employees", employeeDao.query());
 		return "lab/employee";
 	}
+	
+	@PostMapping("/")
+	public String add(@Valid Employee employee, BindingResult result, Model model) {
+		if(result.hasErrors()) {
+			model.addAttribute("_method", "POST");
+			model.addAttribute("employees", employeeDao.query());
+			model.addAttribute("employee", employee);
+			return "lab/employee";
+		}
+		employeeDao.add(employee);
+		return "redirect:./";
+	}
+	
+	@GetMapping("/{eid}")
+	public String get(@PathVariable("eid") Integer eid, Model model) {
+		model.addAttribute("_method", "PUT");
+		model.addAttribute("employees", employeeDao.query());
+		model.addAttribute("employee", employeeDao.get(eid));
+		return "lab/employee";
+	}
+	
+	@PutMapping("/")
+	public String update(@Valid Employee employee, BindingResult result, Model model) {
+		if(result.hasErrors()) {
+			model.addAttribute("_method", "PUT");
+			model.addAttribute("employees", employeeDao.query());
+			model.addAttribute("employee", employee);
+			return "lab/employee";
+		}
+		employeeDao.update(employee);
+		return "redirect:./";
+	}
+	
+	@DeleteMapping("/{eid}")
+	public String delete(@PathVariable("eid") Integer eid) {
+		employeeDao.delete(eid);
+		return "redirect:./";
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/springmvc/lab/repository/EmployeeDaoImpl.java
+++ src/main/java/springmvc/lab/repository/EmployeeDaoImpl.java
@@ -4,14 +4,18 @@ import java.util.List;
 
 import org.simpleflatmapper.jdbc.spring.JdbcTemplateMapperFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.stereotype.Repository;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import springmvc.lab.entity.Employee;
 
 @Repository
 public class EmployeeDaoImpl implements EmployeeDao {
+	private static final int LIMIT = 5;
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 	
@@ -38,32 +42,47 @@ public class EmployeeDaoImpl implements EmployeeDao {
 
 	@Override
 	public List<Employee> queryPage(int offset) {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "select e.eid, e.ename, e.salary, e.createtime, "
+				+ "	j.jid as job_jid, j.jname as job_jname, j.eid as job_eid "
+				+ " from employee e left join job j "
+				+ " on e.eid = j.eid "
+				+ " order by e.eid ";
+		
+		// 分頁查詢
+		if(offset >= 0) {
+			sql += String.format(" limit %d offset %d ", LIMIT, offset);
+		}
+		
+		ResultSetExtractor<List<Employee>> resultSetExtractor = JdbcTemplateMapperFactory.newInstance()
+				.addKeys("eid")
+				.newResultSetExtractor(Employee.class);
+		
+		List<Employee> employees = jdbcTemplate.query(sql, resultSetExtractor);
+		return employees;
 	}
 
 	@Override
 	public Employee get(Integer eid) {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "select eid, ename, salary, createtime from employee where eid=?";
+		return jdbcTemplate.queryForObject(sql, new Object[] {eid}, new BeanPropertyRowMapper<Employee>(Employee.class));
 	}
 
 	@Override
 	public int add(Employee employee) {
-		// TODO Auto-generated method stub
-		return 0;
+		String sql = "Insert into employee(ename, salary) values(?, ?)";
+		return jdbcTemplate.update(sql, employee.getEname(), employee.getSalary());
 	}
 
 	@Override
 	public int update(Employee employee) {
-		// TODO Auto-generated method stub
-		return 0;
+		String sql = "update employee set ename=?, salary=? where eid=?";
+		return jdbcTemplate.update(sql, employee.getEname(), employee.getSalary(), employee.getEid());
 	}
 
 	@Override
 	public int delete(Integer eid) {
-		// TODO Auto-generated method stub
-		return 0;
+		String sql = "delete from employee where eid=?";
+		return jdbcTemplate.update(sql, eid);
 	}
 	
 }

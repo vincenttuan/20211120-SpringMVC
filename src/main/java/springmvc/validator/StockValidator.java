0f80d137f639987d70febf6733722678e54fbb48
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-02-19 14:21:23 +0800

    Update StockValidator.java

--- src/main/java/springmvc/validator/StockValidator.java
+++ src/main/java/springmvc/validator/StockValidator.java
@@ -1,5 +1,6 @@
 package springmvc.validator;
 
+import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
@@ -7,6 +8,7 @@ import org.springframework.validation.Validator;
 import springmvc.entity.Stock;
 import yahoofinance.YahooFinance;
 
+@Component
 public class StockValidator implements Validator {
 
 	@Override
